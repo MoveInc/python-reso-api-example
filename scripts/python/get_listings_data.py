ac343bf4e8e74dd3b70e04eec1e65ca8a5021004
Alex Life <letto4135@gmail.com>
2022-01-05 11:20:49 -0500

    adjust query

--- scripts/python/get_listings_data.py
+++ scripts/python/get_listings_data.py
@@ -15,7 +15,8 @@ time_incremental_cursor = ""
 
 def full_pull(access_token, db_conn, db_cursor):
     global time_incremental_cursor
-    url = "https://api.listhub.com/odata/Property"
+    url = "https://api.listhub.com/odata/Property?$select=Media,"
+    url = url + ",".join([x for x in db_utils.keys if x != "LeadRoutingEmail"]) + ",CustomFields"
     last_access_time = time_incremental_cursor
     time_incremental_cursor = helper_utils.get_current_time_iso()
     properties = helper_utils.get_properties(url, access_token)
@@ -28,9 +29,10 @@ def incremental_run(access_token, db_conn, db_cursor):
     global time_incremental_cursor
     last_access_time = time_incremental_cursor
     time_incremental_cursor = helper_utils.get_current_time_iso()
-    base_url = "https://api.listhub.com/odata/Property"
-    query = "?$filter=ModificationTimestamp gt " + last_access_time
-    url = base_url + query
+    base_url = "https://api.listhub.com/odata/Property?"
+    select = "$select=Media," + ",".join([x for x in db_utils.keys if x != "LeadRoutingEmail"]) + ",CustomFields"
+    query = "&$filter=ModificationTimestamp gt " + last_access_time
+    url = base_url + select + query
     properties = helper_utils.get_properties(url, access_token)
     listingKeys_db = db_utils.get_listingkeys_db(db_conn, db_cursor)
     helper_utils.parse_listings(properties, listingKeys_db, False,
